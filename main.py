6f1ae4a416e0e74d4218e0735852ca3fed98962f
EvgNik88 <evni88@gmail.com>
2024-01-25 13:26:32 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import csv
+
 from playwright.sync_api import sync_playwright
 
 URL = 'https://magento.softwaretestingboard.com/'
@@ -23,7 +24,7 @@ def product_information_parser(page):
 
 
 def initialize_csv_writer():
-    csvfile = open('Magento_Shop_Parser.csv', 'w', newline='')
+    csvfile = open('Magento_Shop_Data.csv', 'w', newline='')
     csv_writer = csv.writer(csvfile)
     csv_writer.writerow(
         ['Item Name', 'Item Price', 'Item Description', 'Material', 'Pattern', 'Climate', 'Colors', 'Style'])
@@ -41,31 +42,30 @@ def goto_category(page, *category_ids):
 
 
 def process_category(page, category_locator, csv_writer):
-    for item in category_locator:
+    for index, item in enumerate(category_locator):
         item.click()
         page.wait_for_selector('//*[@class="swatch-option color"]')
         parsed_info = product_information_parser(page)
         csv_writer.writerow(parsed_info)
-        page.go_back()
+        print(f'{index + 1}/{len(category_locator)} is done...')
+        page.go_back(wait_until='domcontentloaded')
 
     if has_next_page(page):
-        next_page = page.get_by_role('link', name='Next').last
+        next_page = page.locator('//a[@class="action  next"]').last
         next_page.click()
         page.wait_for_load_state('domcontentloaded')
         category_locator = page.locator('//li[contains(@class, "item product")]').all()
         process_category(page, category_locator, csv_writer)
-    else:
-        return
 
 
 def has_next_page(page):
-    next_page = page.get_by_role('link', name='Next').last
-    return next_page.is_visible
+    next_page = page.locator('//a[@class="action  next"]').last
+    return next_page.is_visible()
 
 
-def test_main():
+def main():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
+        browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
@@ -73,21 +73,15 @@ def test_main():
 
         page.goto(URL, wait_until='domcontentloaded')
 
-        hoodies_and_sweatshirts = goto_category(page, 'ui-id-5', 'ui-id-17', 'ui-id-20')
-        process_category(page, hoodies_and_sweatshirts, csv_writer)
+        men_hoodies_and_sweatshirts = goto_category(page, 'ui-id-5', 'ui-id-17', 'ui-id-20')
+        process_category(page, men_hoodies_and_sweatshirts, csv_writer)
 
-        pants = goto_category(page, 'ui-id-5', 'ui-id-18', 'ui-id-23')
-        process_category(page, pants, csv_writer)
-
-        tanks = goto_category(page, 'ui-id-5', 'ui-id-17', 'ui-id-22')
-        process_category(page, tanks, csv_writer)
-
-        bras_and_tanks = goto_category(page, 'ui-id-4', 'ui-id-9', 'ui-id-14')
-        process_category(page, bras_and_tanks, csv_writer)
+        men_pants = goto_category(page, 'ui-id-5', 'ui-id-18', 'ui-id-23')
+        process_category(page, men_pants, csv_writer)
 
         csvfile.close()
         browser.close()
 
 
 if __name__ == "__main__":
-    test_main()
+    main()
